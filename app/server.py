c30eea926b68445ca349dbbd6b8d434dc0ed77b3
Shreya <52953152+ShreyaSrivastav00@users.noreply.github.com>
2019-10-11 13:28:35 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1_7H8Bk2aXhZAc1vPNN8DGJ6RV3mA8_Sh'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1_7H8Bk2aXhZAc1vPNN8DGJ6RV3mA8_Sh'
 export_file_name = 'resnet50.pkl'
 
 classes =['Apple___Apple_scab',
